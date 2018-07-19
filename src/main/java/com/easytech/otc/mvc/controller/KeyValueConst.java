4767767e913590fa5218d5a6ca5b07dce1721456
chenchenghao <459105408@qq.com>
2018-07-20 01:16:32 +0800

    修改注册获取公钥接口

---
+++ src/main/java/com/easytech/otc/mvc/controller/KeyValueConst.java
@@ -0,0 +1,12 @@
+package com.easytech.otc.mvc.controller;
+
+/**
+ * Description:
+ * Author: Hank
+ * Date: 2018/7/20 01:03
+ */
+
+public class KeyValueConst {
+    public static final String PUBLIC_KEY          = "PublicKey";
+    public static final String PRIVATE_KEY         = "PrivateKey";
+}
--- src/main/java/com/easytech/otc/mvc/controller/api/UserController.java
+++ src/main/java/com/easytech/otc/mvc/controller/api/UserController.java
@@ -1,6 +1,7 @@
 package com.easytech.otc.mvc.controller.api;
 
 import com.easytech.otc.common.crypt.RSAUtils;
+import com.easytech.otc.mvc.controller.KeyValueConst;
 import com.easytech.otc.mvc.controller.WebConst;
 import com.easytech.otc.mvc.protocol.ACL;
 import com.easytech.otc.mvc.protocol.Resp;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.security.Key;
 import java.util.Map;
 
@@ -20,25 +21,28 @@ import java.util.Map;
  */
 @RestController
 @RequestMapping(WebConst.API_V1_PREFIX + "/user")
-public class UserController  {
+public class UserController {
     /**
      * 获取公钥
-     * @param request
+     * @param session
      * @return
      */
     @GetMapping(value = "/getPublicKey")
     @ACL(authControl = false)
-    public Resp<String> getSessionId(HttpServletRequest request){
-        Resp<String> resp= new Resp<>();
-        Map<String, Key> keyMap =null;
-        String privateKey = "";
+    public Resp<String> getSessionId(HttpSession session) {
+        String privateKey;
         String publicKey = "";
+        if (session.getAttribute(KeyValueConst.PUBLIC_KEY) != null) {
+            return Resp.newSuccessResult((String) session.getAttribute(KeyValueConst.PUBLIC_KEY));
+        }
+        Resp<String> resp = new Resp<>();
+
         try {
-            keyMap = RSAUtils.initKey();
-            privateKey= RSAUtils.getPrivateKey(keyMap);
+            Map<String, Key> keyMap = RSAUtils.initKey();
+            privateKey = RSAUtils.getPrivateKey(keyMap);
             publicKey = RSAUtils.getPublicKey(keyMap);
-            request.getSession().setAttribute("publickey",publicKey);
-            request.getSession().setAttribute("privateKey",privateKey);
+            session.setAttribute(KeyValueConst.PUBLIC_KEY, publicKey);
+            session.setAttribute(KeyValueConst.PRIVATE_KEY, privateKey);
         } catch (Exception e) {
             e.printStackTrace();
             resp.setFail(RetCodeEnum.INTERNAL_ERROR);
