1d795b507e82bdf0d362f77c99f0c281f3e2d4aa
jiayeee <737093270@qq.com>
2018-07-21 13:28:21 +0800

    统一异常处理

--- src/main/java/com/easytech/otc/mvc/controller/api/RestApiAdvice.java
+++ src/main/java/com/easytech/otc/mvc/controller/api/RestApiAdvice.java
@@ -1,5 +1,6 @@
 package com.easytech.otc.mvc.controller.api;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -19,24 +20,40 @@ public class RestApiAdvice {
     public RespWithoutData catchException(Throwable err) {
         LOGGER.error("rest exception", err);
 
-        RespWithoutData response = new RespWithoutData();
-
-        if (err instanceof BizException) {
-            BizException be = (BizException) err;
-
-            if (be.getBizExceptionEnum() != null) {
-                response.setSystemError();
-                response.setErrorMsg(be.getBizExceptionEnum().getDesc());
-                return response;
-            }
-
+        if (err instanceof IllegalArgumentException) {
+            RespWithoutData response = new RespWithoutData();
             response.setSystemError();
-            response.setErrorMsg(RetCodeEnum.BUSINESS_ERROR.getDesc());
+            response.setErrorMsg(err.getMessage());
             return response;
         }
 
+        if (err instanceof BizException) {
+            BizException be = (BizException) err;
+            RetCodeEnum re = be.getBizExceptionEnum();
+
+            if (re != null) {
+                RespWithoutData response = new RespWithoutData();
+                response.setRetCode(re.getCode());
+                response.setErrorMsg(re.getDesc());
+                return response;
+            }
+
+            return newResp(StringUtils.isNotBlank(be.getMessage()) ? be.getMessage() : RetCodeEnum.BUSINESS_ERROR.getDesc());
+        }
+
+        return newResp(null);
+    }
+
+    private RespWithoutData newResp(String msg) {
+        RespWithoutData response = new RespWithoutData();
+
         response.setSystemError();
-        response.setErrorMsg(RetCodeEnum.UNKOWN.getDesc());
+        if (StringUtils.isNotBlank(msg)) {
+            response.setErrorMsg(msg);
+        } else {
+            response.setErrorMsg(RetCodeEnum.UNKOWN.getDesc());
+        }
+
         return response;
     }
 }
